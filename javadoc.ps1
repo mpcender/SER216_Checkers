7fe152a4ba659d2c5aaa114979b2ba5988dd09f9
Matt Cummings <mpc.ender@gmail.com>
2021-06-05 14:09:09 -0700

    modify javadoc powershell command

--- javadoc.ps1
+++ javadoc.ps1
@@ -1,2 +1,2 @@
-cd C:\Users\sephi\Desktop\ASU\SER216\SER216_Projects\Checkers
-javadoc -d C:\Users\sephi\Desktop\ASU\SER216\SER216_Projects\Week_3\Checkers\docs -sourcepath C:\Users\sephi\Desktop\ASU\SER216\SER216_Projects\Checkers\src -subpackages core:ui
+cd C:\Users\sephi\Desktop\ASU\SER216\Checkers
+javadoc -d C:\Users\sephi\Desktop\ASU\SER216\Checkers\src\docs -sourcepath C:\Users\sephi\Desktop\ASU\SER216\Checkers\src -subpackages core:ui
